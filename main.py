512a43ea2e9f76c6673509d396aef4c772f5e182
Jasraj Bedi <jasrajbedi@jasrajs-mbp.wireless.concordia.ca>
2019-03-27 01:20:17 -0400

    Created 500 list of patents as csv and got the top assignees.

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import bq_helper
 import os
 from bq_helper import BigQueryHelper
+import matplotlib.pyplot as plt
+from scipy import ndimage
 # https://www.kaggle.com/sohier/introduction-to-the-bq-helper-package
-
-
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "./My First Project-0ad1d9baf9e6.json"
 
 bq_assistant = BigQueryHelper("patents-public-data", "patents")
-a = bq_assistant.list_tables()
-rows = bq_assistant.head("publications_201710", num_rows=800)
+table_names = bq_assistant.list_tables()
+print(table_names)
+rows = bq_assistant.head("publications_201809", num_rows=500)
 
-print(type(rows))
+assignees = rows["assignee"].value_counts()
+
+
+def top_n_assignees(n):
+    return assignees.nlargest(n)
+
+
+print(top_n_assignees(5))
+top_n_assignees(5).hist()
+plt.show()
