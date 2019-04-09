a5cbc21630585c391457c0ac83ada6c6b63b5273
jasrajbedi <jasraj3453@gmail.com>
2019-04-08 20:41:13 -0400

    tf-idf words

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bq_helper import BigQueryHelper
 from pyspark_dist_explore import hist
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
-
+import pandas as pd
 import sys
 import copy
 import time
@@ -12,15 +12,19 @@ import random
 import pyspark
 from statistics import mean
 from pyspark.rdd import RDD
+import numpy as np
 from pyspark.sql import Row
 from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
+from nlp_functions import get_important_nouns_from_a_string, list_dict_representation_to_actual_list_dict
+from nlp_functions import get_data_tfidf_weights_and_vectorizer_from_corpus
 from pandas.plotting import scatter_matrix
 from pyspark.ml.fpm import FPGrowth
 from pyspark.sql.functions import lit, desc, size, max, col, abs, monotonically_increasing_id
 from pyspark.sql.types import StructType, StructField, StringType
 from scipy import ndimage
 # https://www.kaggle.com/sohier/introduction-to-the-bq-helper-package
+
 
 '''
 HELPER FUNCTIONS
@@ -64,6 +68,8 @@ def toCSVLine(data):
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "./My First Project-0ad1d9baf9e6.json"
 
+min_support = 0.1
+confidence = 0.3
 bq_assistant = BigQueryHelper("patents-public-data", "patents")
 table_names = bq_assistant.list_tables()
 print("\nTable names:\n")
@@ -80,8 +86,40 @@ data_from_csv = spark.read.csv("publications_201809.csv", header=True).rdd
 data_from_csv = spark.sparkContext.parallelize(data_from_csv.collect())
 
 filtered_data = data_from_csv.filter(lambda x: len(x.assignee) > 2 and len(x.abstract_localized) > 2 and len(x.inventor) > 2)
-filtered_data_dataframe = filtered_data.toDF()
-inventors = filtered_data_dataframe.groupBy('assignee').count().sort(col("count").desc()).show()
+
+abstract_rdd = filtered_data.map(lambda x: (x.publication_number, list_dict_representation_to_actual_list_dict(x.abstract_localized, "text")))
+full_list = abstract_rdd.collect()
+full_list = list(map(lambda x: x[1], full_list))
+# print(full_list)
+result = get_data_tfidf_weights_and_vectorizer_from_corpus(full_list)
+arr = result[0].toarray()
+v = result[1]
+for inside_arr in arr:
+    result = np.where(inside_arr > 0.2)
+    for r in result:
+        for c in r:
+            print(v[c])
+# print(v)
+# print(arr.toarray().shape)
+# for i in arr.toarray():
+#     for j in i:
+#         print(j)
+
+# words_rdd = filtered_data.map(lambda x: (x.publication_number,
+#                                          get_important_nouns_from_a_string(list_dict_representation_to_actual_list_dict(x.abstract_localized, "text"))))
+# words_dataframe = words_rdd.toDF()
+# words_dataframe = words_dataframe.selectExpr("_1 as Publication_number", "_2 as Important_words")
+# words_dataframe.show()
+# fpgrowth = FPGrowth(itemsCol="Important_words", minSupport=min_support, minConfidence=confidence)
+# model = fpgrowth.fit(words_dataframe)
+# my_dataframe = model.freqItemsets.orderBy("freq", ascending=False)
+# my_dataframe.show()
+
+
+# filtered_data_dataframe = filtered_data.toDF()
+#
+# inventors = filtered_data_dataframe.groupBy('assignee').count().sort(col("count").desc()).show()
+
 
 # hist(ax, inventors, bins=20, color=['red'])
 # data_dataframe = filtered_data.toDF()
---
+++ nlp_functions.py
@@ -0,0 +1,25 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+import ast
+
+
+
+def get_important_nouns_from_a_string(string):
+    token = nltk.word_tokenize(string)
+    result = nltk.pos_tag(token)
+    filtered_result = list(filter(lambda x: x[1] == "NN" or x[1] == "NNP", result))
+    ans = list(map(lambda x: x[0], filtered_result))
+    return list(set(ans))
+
+
+def list_dict_representation_to_actual_list_dict(string, dict_key):
+    ans = ast.literal_eval(string)
+    return ans[0][dict_key]
+
+
+def get_data_tfidf_weights_and_vectorizer_from_corpus(corpus):
+    v = TfidfVectorizer(lowercase=True, analyzer='word', use_idf=True)
+    response = v.fit_transform(corpus)
+    return response, v.get_feature_names()
+
