7b9d9cdeebee4945ba6d954e103ef68b549543e8
jasrajbedi <jasraj3453@gmail.com>
2019-04-13 03:19:08 -0400

    pca with kmeans clustering

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from pyspark.ml.evaluation import ClusteringEvaluator
 from nlp_functions import get_important_nouns_from_a_string, list_dict_representation_to_actual_list_dict
 from nlp_functions import get_data_tfidf_weights_and_vectorizer_from_corpus
 from pyspark.sql import SparkSession
+from pyspark.ml.feature import PCA as PCAml
+from pyspark.ml.linalg import Vectors
 from pyspark.sql import SQLContext
+from pyspark.sql.functions import lit
 from pyspark.sql.types import *
+from pyspark.ml.feature import VectorAssembler
 import numpy as np
 import pandas as pd
+
+def is_ascii(s):
+    return all(ord(c) < 128 for c in s)
 
 def init_spark():
     spark = SparkSession \
@@ -17,29 +24,43 @@ def init_spark():
     return spark
 
 spark = init_spark()
+spark.conf.set("spark.sql.crossJoin.enabled", "true")
 
 
 data_from_csv = spark.read.csv("publications_201809.csv", header=True).rdd
 data_from_csv = spark.sparkContext.parallelize(data_from_csv.collect())
 # Filtering the columns where there are empty assignees , inventors or abstracts.
 filtered_data = data_from_csv.filter(lambda x: len(x.assignee) > 2 and len(x.abstract_localized) > 2 and len(x.inventor) > 2)
+publication_num = filtered_data.map(lambda x: x.publication_number).collect()
 
-abstract_rdd = filtered_data.map(lambda x: (x.publication_number, list_dict_representation_to_actual_list_dict(x.abstract_localized, "text")))
+abstract_rdd = filtered_data.map(lambda x: (x.publication_number, list_dict_representation_to_actual_list_dict(x.description_localized, "text")))
 full_list = abstract_rdd.collect()
 full_list = list(map(lambda x: x[1], full_list))
 result = get_data_tfidf_weights_and_vectorizer_from_corpus(full_list)
-arr = result[0].toarray()
+
 word_list = result[1]
+print(len(word_list))
+index_list_to_remove_from_word_list = [w[0] for w in enumerate(word_list) if is_ascii(w[1]) and w[1].isalpha()]
+arr = result[0].toarray()
+print(type(arr))
+print(arr.shape)
+arr_filtered = np.delete(arr, index_list_to_remove_from_word_list, axis=1)
+# print(index_list_to_remove_from_word_list)
 
 reference_list = []
 counter = 0
-for inside_arr in arr:
+for inside_arr in arr_filtered:
+    tfidf_weight_list = inside_arr.tolist()
+    reference_list.append([publication_num[counter]] + inside_arr.tolist())
     counter = counter + 1
-    reference_list.append([counter] + inside_arr.tolist())
+
+print(reference_list)
+
 
 columns_headings = [str(i[0]+1) for i in enumerate(inside_arr)]
-print(columns_headings)
-column_heading_zero = ["label"]
+
+
+column_heading_zero = ["patent"]
 
 
 pandas_df = pd.DataFrame(reference_list)
@@ -53,24 +74,41 @@ mySchema = StructType([StructField("labels", IntegerType(), True), StructField("
 
 df = spark.createDataFrame(reference_list,
                               column_heading_zero+columns_headings)
-from pyspark.ml.feature import VectorAssembler
+
 
 vecAssembler = VectorAssembler(inputCols=columns_headings, outputCol="features")
 new_df = vecAssembler.transform(df)
 
+publication_num_df = new_df.select("patent")
+publication_num_df = publication_num_df.withColumn("index", lit("7"))
+publication_num_df.show()
+
+
+
+# Pca
+
+pca = PCAml(k=2, inputCol="features", outputCol="pca")
+model = pca.fit(new_df.select('features'))
+transformed = model.transform(new_df.select('features'))
+
+new_df = transformed.select("pca")
+new_df = new_df.withColumn("index", lit("7"))
+new_df.show()
+ns = publication_num_df.join(new_df, new_df.index == publication_num_df.index).select(publication_num_df.patent, new_df.pca)
+ns = ns.selectExpr("patent as patent", "pca as features")
+ns.show()
 
 
 
 
-
-#
 # # Trains a k-means model.
 kmeans = KMeans().setK(5).setSeed(1)
-model = kmeans.fit(new_df.select('features'))
+model = kmeans.fit(ns.select('features'))
+
 #
 # # Make predictions
-predictions = model.transform(new_df)
-predictions.show()
+predictions = model.transform(ns)
+# predictions.show()
 #
 # # Evaluate clustering by computing Silhouette score
 evaluator = ClusteringEvaluator()
@@ -83,3 +121,5 @@ centers = model.clusterCenters()
 print("Cluster Centers: ")
 for center in centers:
     print(center)
+
+
